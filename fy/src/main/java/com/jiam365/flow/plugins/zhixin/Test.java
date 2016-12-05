998bc81b224eb5aa3fd783f8989e338c06b7706c
shenji <121659050>
2016-12-05 13:11:48 +0800

    fy xinkedang

---
+++ fy/src/main/java/com/jiam365/flow/plugins/zhixin/Test.java
@@ -0,0 +1,73 @@
+//package com.jiam365.flow.plugins.yuanju;
+//
+//import com.jiam365.flow.sdk.response.ResponseData;
+//import com.jiam365.flow.sdk.response.XMLDataReader;
+//import com.jiam365.modules.utils.StringIdGenerator;
+//import org.apache.http.client.methods.HttpGet;
+//
+//import java.io.UnsupportedEncodingException;
+//import java.text.SimpleDateFormat;
+//
+//public class Test {
+//	private static String MARK = "UC";
+//	private static String rechargeUrl = "http://120.55.88.183:6001/datapay/api/yuanju/charge";
+//	private static String key = "c1e192d298f745d4b2f4352735f9e61e";
+//	private static String username = "szhh";
+//    private static String secret = "9ed411c5ae70413aaf7ed547bf5a410a";
+//	private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+//
+//	public static void main(String[] args) {
+////		createOrders("13402565476", dateFormat.format(new Date()), "0", "10");
+//
+////		String sign = MD5.SHA1("aaaaa,15939393939|13878787878,10,20,20160223120445,7b21848ac9af35be0ddb2d6b9fc3851934db8420");
+////		sign.toString();
+//	}
+//
+//	private static ResponseData createOrders(String phone, String timestamp,String area, String capacity) {
+//		OrderCreateRequestDTO dto = new OrderCreateRequestDTO();
+//		dto.setUsername(username);
+//		dto.setKey(key);
+//		String orderId = StringIdGenerator.get();
+//		dto.setClientid(orderId);
+//		dto.setProductids(request.getProductId());
+//		dto.setMobile(request.getMobile());
+//		dto.generateSignature(secret);
+//		dto.setNotifyurl("http://120.55.71.93/report/charge");
+//		dto.setFlowtype(getArea(request.getProductId()));
+//		String url="";
+//		url=rechargeUrl+"?username="+dto.getUsername()+
+//				"&key="+dto.getKey()+
+//				"&clientid="+dto.getClientid()+
+//				"&productids="+dto.getProductids()+
+//				"&mobile="+dto.getMobile()+
+//				"&sign="+dto.getSign()+
+//				"&notifyurl="+dto.getNotifyurl()+
+//				"&flowtype="+dto.getNotifyurl();
+//
+//		HttpGet method = ClientUtils.getGetMethod(url);
+//		System.out.println(MARK + "_recharge_url:" + url);
+//		String o = ClientUtils.getJson(method);
+//
+//		try {
+//			o = new String(o.getBytes("ISO-8859-1"), "utf8");
+//		} catch (UnsupportedEncodingException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		System.out.println(MARK + "_recharge_xml:" + o);
+//		XMLDataReader reader = new XMLDataReader();
+//		reader.init(o);
+//		String ret_code = reader.read("success");
+//		String ret_msg = reader.read("message");
+//		String req_order_number = reader.read("partner_order_no");
+//		ResponseData data = new ResponseData();
+//		data.setSuccessValue("0");
+//		data.setResult(ret_code);
+//		data.setMessage(ret_msg);
+//		System.out.println(ret_msg);
+//		data.setRequestNo(req_order_number);
+//		return data;
+//	}
+//
+//}
---
+++ fy/src/main/java/com/jiam365/flow/plugins/zhixin/ZhiXinCallbackController.java
@@ -0,0 +1,55 @@
+package com.jiam365.flow.plugins.zhixin;
+
+import com.jiam365.flow.sdk.support.TradeReportServiceProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+
+@Controller
+@RequestMapping(value = "/report")
+public class ZhiXinCallbackController {
+
+    private static Logger logger = LoggerFactory.getLogger(ZhiXinCallbackController.class);
+
+    @RequestMapping(value = "charge")
+    @ResponseBody
+    public String callback(HttpServletRequest request) {
+        StringBuffer jb = new StringBuffer();
+        String line = null;
+        try {
+            BufferedReader reader = request.getReader();
+            while ((line = reader.readLine()) != null)
+                jb.append(line);
+        } catch (Exception e) {
+            /* report an error */
+        }
+
+        String json = jb.toString().trim();
+        return parse(json);
+    }
+
+    public String parse(String json) {
+        logger.debug("收到志新公司回调报文 {}", json);
+
+        if (!StringUtils.isEmpty(json)) {
+            try {
+                ZhiXinReport report = ClientUtils.getJsonMapper().fromJson(json, ZhiXinReport.class);
+                if(report.getData() != null) {
+                    TradeReportServiceProxy.save(report.getData().messageid, json);
+                    return "ok";
+                }
+               return "fail";
+            } catch (Exception e) {
+                return "fail";
+            }
+        } else {
+            return "fail";
+        }
+    }
+}
