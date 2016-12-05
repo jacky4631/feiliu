80082965e54aea0e7cf6d503f2d495972dd7aaec
jacky4631 <jacky4631@qq.com>
2016-12-05 23:33:17 +0800

    zhixin

--- zhixin/src/main/java/com/jiam365/flow/plugins/zhixin/ZhiXinHandler.java
+++ zhixin/src/main/java/com/jiam365/flow/plugins/zhixin/ZhiXinHandler.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -51,7 +52,7 @@ public class ZhiXinHandler extends AbstractHandler {
 		String o = ClientUtils.getJson(method);
 		logger.debug(MARK + "_recharge_url ret:" + o);
 		try {
-			o = new String(o.getBytes("ISO-8859-1"), "utf8");
+			o = URLDecoder.decode(o,"UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
