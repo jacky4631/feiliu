5d0ae94304ae7738b03af8c5b2e3250ecb2b04bf
shenji <121659050>
2016-12-09 15:53:46 +0800

    qweishu

---
+++ qiweishu/src/main/java/com/jiam365/flow/plugins/qiweishu/OrderCreateResponseDTO.java
@@ -0,0 +1,47 @@
+package com.jiam365.flow.plugins.qiweishu;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * Created by ken on 15/12/13.
+ */
+public class OrderCreateResponseDTO {
+	
+	private boolean success;
+	private String error;
+	private String orderId;
+	private int price;
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
