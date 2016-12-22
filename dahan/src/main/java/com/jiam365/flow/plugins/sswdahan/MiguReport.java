889496f1b19134f681c0e4e9ab47f0fad20540d1
shenji <121659050>
2016-12-22 10:30:49 +0800

    dahan

---
+++ dahan/src/main/java/com/jiam365/flow/plugins/sswdahan/MiguReport.java
@@ -0,0 +1,50 @@
+package com.jiam365.flow.plugins.sswdahan;
+
+public class MiguReport {
+
+	private String orderNo;
+	private String orderNumber;
+	private String mobile;
+	private String status;
+	private String message;
+
+	public String getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
---
+++ dahan/src/main/java/com/jiam365/flow/plugins/sswdahan/Params.java
@@ -0,0 +1,34 @@
+package com.jiam365.flow.plugins.sswdahan;
+
+/**
+ * Created by 沈吉 on 2016/12/9.
+ */
+public class Params {
+    private String rechargeUrl;
+    private String account;
+    private String apiKey;
+
+    public String getRechargeUrl() {
+        return rechargeUrl;
+    }
+
+    public void setRechargeUrl(String rechargeUrl) {
+        this.rechargeUrl = rechargeUrl;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+}
