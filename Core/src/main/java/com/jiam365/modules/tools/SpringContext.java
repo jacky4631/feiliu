28e86168834badee2d48014dc19d656466889f38
shenji <121659050>
2016-12-23 15:38:27 +0800

    feiliudec

--- Core/src/main/java/com/jiam365/modules/tools/SpringContext.java
+++ Core/src/main/java/com/jiam365/modules/tools/SpringContext.java
@@ -3,6 +3,7 @@ package com.jiam365.modules.tools;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
@@ -12,50 +13,41 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationEvent;
 
 public class SpringContext
-        implements ApplicationContextAware
-{
+        implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
 
-    public void setApplicationContext(ApplicationContext applicationContext)
-    {
-        applicationContext = applicationContext;
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
     }
 
-    public static ApplicationContext getApplicationContext()
-    {
+    public static ApplicationContext getApplicationContext() {
         checkApplicationContext();
         return applicationContext;
     }
 
-    public static <T> T getBean(String name)
-    {
+    public static <T> T getBean(String name) {
         checkApplicationContext();
-        return (T)applicationContext.getBean(name);
+        return (T) applicationContext.getBean(name);
     }
 
-    public static <T> T getBean(Class<T> clazz)
-    {
+    public static <T> T getBean(Class<T> clazz) {
         checkApplicationContext();
         Map beans = applicationContext.getBeansOfType(clazz);
-        if ((beans != null) && (!beans.isEmpty()))
-        {
+        if ((beans != null) && (!beans.isEmpty())) {
             Iterator localIterator = beans.keySet().iterator();
-            if (localIterator.hasNext())
-            {
+            if (localIterator.hasNext()) {
                 Object o = localIterator.next();
-                return (T)beans.get(o);
+                return (T) beans.get(o);
             }
         }
         return null;
     }
 
-    public static void registerBean(String className, String initMethodName, String destroyMethodName)
-    {
-        DefaultListableBeanFactory acf = (DefaultListableBeanFactory)applicationContext.getAutowireCapableBeanFactory();
+    public static void registerBean(String className, String initMethodName, String destroyMethodName) {
+        DefaultListableBeanFactory acf = (DefaultListableBeanFactory) applicationContext.getAutowireCapableBeanFactory();
 
         String beanId = getBeanId(className);
-        if (getBean(beanId) == null)
-        {
+        if (getBean(beanId) == null) {
             BeanDefinitionBuilder bdb = BeanDefinitionBuilder.rootBeanDefinition(className);
             if (StringUtils.isNotBlank(initMethodName)) {
                 bdb.setInitMethodName(initMethodName);
@@ -68,23 +60,20 @@ public class SpringContext
         }
     }
 
-    public static void removeBean(String className)
-    {
+    public static void removeBean(String className) {
         String beanId = getBeanId(className);
-        DefaultListableBeanFactory acf = (DefaultListableBeanFactory)applicationContext.getAutowireCapableBeanFactory();
+        DefaultListableBeanFactory acf = (DefaultListableBeanFactory) applicationContext.getAutowireCapableBeanFactory();
         if (acf.containsBean(beanId)) {
             acf.removeBeanDefinition(beanId);
         }
     }
 
-    public static void fireEvent(ApplicationEvent event)
-    {
+    public static void fireEvent(ApplicationEvent event) {
         checkApplicationContext();
         applicationContext.publishEvent(event);
     }
 
-    private static String getBeanId(String className)
-    {
+    private static String getBeanId(String className) {
         int pos = className.lastIndexOf('.');
         String commName = className.substring(pos + 1);
         String firstLetter = commName.substring(0, 1);
@@ -92,8 +81,7 @@ public class SpringContext
         return firstLetter.toLowerCase() + commName.substring(1);
     }
 
-    private static void checkApplicationContext()
-    {
+    private static void checkApplicationContext() {
         if (applicationContext == null) {
             throw new IllegalStateException("applicaitonContext not set, Please define the SpringContextHolder bean in applicationContext.xml.");
         }
